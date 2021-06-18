a5c24f3bbdead94dbc5751d0d58af1c12a9bbb4e
Jim Forristal <nwclassic@gmail.com>
2021-06-18 13:22:00 -0700

    working version to change AMI image type

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # modify-EC2-instance
 
-This will modify a simple EC2 instance.
-
+This will change the AMI of a simple EC2 instance.
+Actually it will delete the original and replace it with the new one.
 
--- main.tf
+++ main.tf
@@ -14,13 +14,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
- resource "aws_instance" "app_server" {
--  ami           = "ami-830c94e3"
-+  ami           = "ami-08d70e59c07c61a3a"
+resource "aws_instance" "app_server" {
+   ami           = "ami-08d70e59c07c61a3a"
    instance_type = "t2.micro"
- }
 
-  tags = {
+ tags = {
     Name = "ExampleAppServerInstance"
   }
 }
