1c9512902e03d6385ce95ca7276471a6a5f952f3
Jim Forristal <nwclassic@gmail.com>
2021-06-18 12:30:38 -0700

    modified main.tf to change ami

--- main.tf
+++ main.tf
@@ -14,9 +14,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+ resource "aws_instance" "app_server" {
+-  ami           = "ami-830c94e3"
++  ami           = "ami-08d70e59c07c61a3a"
+   instance_type = "t2.micro"
+ }
 
   tags = {
     Name = "ExampleAppServerInstance"
